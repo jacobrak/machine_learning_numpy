007de4337b676e8009d31593455b6e5048751be8
jacobrak <155147415+jacobrak@users.noreply.github.com>
2026-01-16 18:39:05 +0100

    dataset updated

--- _create_dataset.py
+++ _create_dataset.py
@@ -7,20 +7,23 @@ def _create_dataset(
     n_samples = 1000,
     beta0 = 5,      # Intercept
     beta1 = 2.5,    # Coefficient for X1
-    beta2 = -1.2    # Coefficient for X2
+    beta2 = -1.2,   # Coefficient for X2
     ):
-
 
     X = np.random.uniform(0, 10, size=(n_samples, 3))
 
+    X4 = X[:, 0] + np.random.normal(0, 0.1, n_samples)
+    X5 = np.random.uniform(0, 10, n_samples)
     # y = beta0 + (beta1 * X1) + (beta2 * X2) + noise
     noise = np.random.normal(0, 1, n_samples)  # Adds some realism
-    y = beta0 + (beta1 * X[:, 0]) + (beta2 * X[:, 1]) + noise
+    y = beta0 + (beta1 * X[:, 0]) + (beta2 * X[:, 1]) + (X[:, 1] *X[:, 2]) + noise
 
     df = pd.DataFrame({
         'X1': X[:, 0],
         'X2': X[:, 1],
         'X3': X[:, 2],
+        'X4': X4,
+        "X5": X5,
         'y': y
     })
     df.to_csv("df.csv")
